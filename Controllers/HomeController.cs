751b705ba0f17245a19deb0b6c8c16d88e120e91
Miteshl Jha <mitesh@vijatshisoftware.com>
2021-01-26 13:16:56 +0530

    kk

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace MVCDemo.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class HomeController : Controller
     {
         public ActionResult Index()
