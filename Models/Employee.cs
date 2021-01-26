30b3bb0a20a5a8ef8b2a1578428441b274eea67a
Miteshl Jha <mitesh@vijatshisoftware.com>
2021-01-26 13:13:13 +0530

    Initial Commit

---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+
+        public int DepartmentId { get; set; }
+    }
+}
